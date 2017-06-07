5015c9a84197760ea3b81042dae88166ec1ebc2d
Tommy Tsang <tommy.tsang@local.dev>
2017-06-07 17:43:55 +0800

    2 chat rooms

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/chat', (req, res) => {
+app.get('/chat1', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/chat2', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
@@ -24,19 +28,29 @@ io.on('connection', (socket) => {
 	var usr;
 	var room;
 
-	online_user.room = [];
+	socket.on('exist user', (username, userToken, r) => {
+		if(typeof online_user[r] === 'undefined'){
+			online_user[r] = new Array();
+		}
 
-	socket.on('exist user', (username, userToken) => {
-		io.emit('exist', username.trim() === "" || ( online_user.room.indexOf(username) > -1 ), userToken );
+		io.emit('exist', username.trim() === "" || ( online_user[r].indexOf(username) > -1 ), userToken );
 	});
 
 	socket.on('enter', (username, r) => {
 		room = r;
+
+		if(typeof online_user[r] === 'undefined'){
+			online_user[room] = new Array();
+		}
+
 		socket.join(room);
 
+		console.log(room);
+		console.log(online_user[room]);
+
 		usr = username;
-		online_user.room.push(usr);
-		io.to(room).emit('online user', online_user.room);
+		online_user[room].push(usr);
+		io.to(room).emit('online user', online_user[room]);
 		io.to(room).emit('notice message', usr+' connected');	
 	});
 
@@ -63,8 +77,8 @@ io.on('connection', (socket) => {
 
 	socket.on('disconnect', () => {	
 		if(usr !== undefined){
-			online_user.room.splice(online_user.room.indexOf(usr), 1);
-			io.to(room).emit('online user', online_user.room);
+			online_user[room].splice(online_user[room].indexOf(usr), 1);
+			io.to(room).emit('online user', online_user[room]);
 			io.to(room).emit('notice message', usr+' disconnected');	
 		}
 	});
