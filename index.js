82f5ea34bfc8469c69b18cc2b8c4de5285853a2d
Tommy Tsang <tommy.tsang@local.dev>
2017-06-08 10:47:38 +0800

    Complete private message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {serveClient: true});
 const dateFormat = require('dateformat');
 const sanitizeHtml = require('sanitize-html');
 const url = require('url');
@@ -9,14 +9,14 @@ const url = require('url');
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
+	res.redirect('/chat-room-'+Math.floor(Math.random()*2 + 1));
+});
+
+app.get('/chat-room-1', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/chat1', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/chat2', (req, res) => {
+app.get('/chat-room-2', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
@@ -33,6 +33,13 @@ io.on('connection', (socket) => {
 			online_user[r] = new Array();
 		}
 
+		username = sanitizeHtml(username, {
+		  allowedTags: [],
+		  allowedAttributes: []
+		});
+
+		username = username.replace(/[^a-zA-Z0-9]/g,'_');
+
 		io.emit('exist', username.trim() === "" || ( online_user[r].indexOf(username) > -1 ), userToken );
 	});
 
@@ -45,20 +52,32 @@ io.on('connection', (socket) => {
 
 		socket.join(room);
 
-		console.log(room);
-		console.log(online_user[room]);
+		//change all characters except numbers and letters
+		username = username.replace(/[^a-zA-Z0-9]/g,'');
 
 		usr = username;
 		online_user[room].push(usr);
 		io.to(room).emit('online user', online_user[room]);
 		io.to(room).emit('notice message', usr+' connected');	
+
+		//Get a list of client IDs connected
+		io.in(room).clients((err, clients) => {
+			if(err) throw err;
+			//console.log("All clients in "+room);
+			//console.log(clients);
+		});
+
+		//Assign key <=> value to username <=> socket.id
+		online_user[r][username] = socket.id;
+		//console.log("Socket: ");
+		//console.log(socket.id);
 	});
 
 	socket.on('leave', () => {
 		io.to(room).emit('notice message', usr+' disconnected');
 	});
 
-	socket.on('chat message', (username, msg) => {
+	socket.on('chat message', (username, msg, receiver) => {
 
 		msg = sanitizeHtml(msg, {
 		  allowedTags: [],
@@ -67,8 +86,15 @@ io.on('connection', (socket) => {
 
 		if(msg.trim() !== ""){
 			var date = Date();
-			io.to(room).emit('chat message', "<small style='font-weight:300;'>"+dateFormat(date, "HH:MM:ss dd/mm/yyyy") +"</small>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +username+": "+msg );
+			if(receiver === "All"){
+				io.to(room).emit('chat message', "<small style='font-weight:300;'>"+dateFormat(date, "HH:MM:ss dd/mm/yyyy") +"</small>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +username+": "+msg );
+			}else{
+				io.to(online_user[room][receiver]).to(online_user[room][username]).emit('chat message', "<small style='font-weight:300;'>"+dateFormat(date, "HH:MM:ss dd/mm/yyyy") +"</small>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(private) " +username+": "+msg );
+			}
 		}
+
+		console.log(receiver);
+		console.log(online_user[room][receiver]);
 	});
 
 	socket.on('typing', (username) => {
@@ -79,7 +105,7 @@ io.on('connection', (socket) => {
 		if(usr !== undefined){
 			online_user[room].splice(online_user[room].indexOf(usr), 1);
 			io.to(room).emit('online user', online_user[room]);
-			io.to(room).emit('notice message', usr+' disconnected');	
+			io.to(room).emit('notice message', usr+' disconnected');
 		}
 	});
 }); 
